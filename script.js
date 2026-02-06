ba13319293459751f444c51af9bbc218573b6043
Esteban Mercado <esteban.mercadom99@gmail.com>
2026-02-05 19:11:39 -0600

    agregando funcionalidad de auto reload si no es un número

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let tempC = +prompt("Ingresa la temperatura en grados celcius")
-
-
 do {
-    if (Number.isFinite(tempC)) {
 
+    let tempC = +prompt("Ingresa la temperatura en grados celcius")
+    if (Number.isFinite(tempC)) {
         let div = document.getElementById("respuesta")
         div.innerHTML = `En kelvin: ${tempC + 273.15}; en Farenheit ${(tempC * 9 / 5) + 32}`
+    } else {
+        alert("No es un número, intenta de nuevo")
+        window.location.reload()
     }
 } while (!Number.isFinite(tempC))
